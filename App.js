5caa4c2cb11745af1dc775a18b576849b19404f7
chattavat.macOs <chattavat.v@gmail.com>
2020-02-03 18:14:35 +0700

    build startGameScreen and build Card component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import Header from './components/Header';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      <Header title="Guess a Number"/>
+      <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
