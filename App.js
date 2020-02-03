e35032e5ecd82f91bf6383b0dcf42628602ad998
chattavat.macOs <chattavat.v@gmail.com>
2020-02-03 11:45:48 +0700

    build list item after input value

--- App.js
+++ App.js
@@ -2,13 +2,34 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Hello work React Native");
+  const [enterGoal, setEnterGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnterGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enterGoal]);
+  };
 
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Input..." style={styles.input} />
-        <Button title="ADD" />
+        <TextInput
+          placeholder="Input..."
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enterGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {courseGoals.map((goal, index) => (
+          <View style={styles.listItem} key={index}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -30,5 +51,13 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     width: "80%"
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+    backgroundColor: "#ccc",
+    borderColor: "grey",
+    borderWidth: 1
   }
 });
