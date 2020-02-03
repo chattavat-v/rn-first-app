6141c0b080d1a7a8146d72d7b0dfb077ff4c06d0
chattavat.macOs <chattavat.v@gmail.com>
2020-02-03 17:16:17 +0700

    build header component and import into app

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Hello work React Native");
-
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Hello world" onPress={() => setOutputText('Hello word CHANGE')} />
+    <View style={styles.screen}>
+      <Header title="Guess a Number"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    flex: 1
+  }
 });
